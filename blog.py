3759ffaf425f0c829adee46ed55716cadd7080bc
IKECHIKE <ronteras@gmail.com>
2014-11-30 00:37:40 +0100

    added basic templates and blog.py code

--- blog.py
+++ blog.py
@@ -1,1 +1,30 @@
  
+from flask import Flask, render_template, request, session, \
+ 	flash, redirect, g, url_for
+
+import sqlite3
+
+#configuration
+DATABASE = 'blog.db'
+
+app = Flask(__name__)
+
+#pulls in app configuration by looking for UPPERCASE variables
+app.config.from_object(__name__)
+
+#function used for connecting to the db
+def connect_db():
+	return sqlite3.connect(app.config['DATABASE'])
+
+@app.route('/')
+def login():
+	return render_template('login.html')
+
+@app.route('/main')
+def main():
+	return render_template('main.html')
+
+
+if __name__ == '__main__':
+	app.run(debug=True)
+
